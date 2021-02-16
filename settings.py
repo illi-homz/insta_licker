e917f10b16fa8ba96ba9ba3653976011919c1df3
illi-homz <gopkabel@mail.ru>
2021-02-16 20:36:30 +0300

    set settings

--- settings.py
+++ settings.py
@@ -1,3 +1,3 @@
-user_login = 'nastya_pro_sugaring'
-user_password = 'fufik123567chupachups'
-client_url = 'https://www.instagram.com/nelli_cosmo/'
+user_login = 'some_login'
+user_password = 'login_password'
+client_url = 'https://www.instagram.com/<target_url>/'
